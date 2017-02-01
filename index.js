dd7e3aac83aaa80ce2434f025be2e74ec7fa9eac
Aden Naheen <elpaperboy@gmail.com>
2017-01-31 19:38:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var server =  express();
+var port = process.env.PORT || 8080;
+
+//to serve css, images, and js
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+response.sendFile('index.html', {root: __dirname + '/public/html'});
+});
+
+server.listen(port, function(){
+   console.log('Now listening on port...', port);
+
+});
